69b8194d58dc59d467424fb37463625a7e9636c8
andrikonon <andri.konon@gmail.com>
2024-04-28 10:53:00 +0200

    Begin working client

--- 1. Simple Socket_Server/Program.cs
+++ 1. Simple Socket_Server/Program.cs
@@ -6,14 +6,42 @@ namespace _1._Simple_Socket_Server;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+        
+        var hostName = Dns.GetHostName();
+        Console.WriteLine($"This machine is {hostName}");
+
+        IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+
+        int selectIP = 0;
+        int i = 0;
+        Console.WriteLine("0. localhost");
+        foreach (var address in localhost.AddressList)
+        {
+            await Console.Out.WriteLineAsync($"{++i}. {address.ToString()}");
+        }
+
+
+        Console.Write(">> ");
+        selectIP = int.Parse(Console.ReadLine()) - 1;
+        IPAddress ip;
+        if (selectIP == -1)
+        {
+            ip = IPAddress.Parse("127.0.0.1");
+        }
+        else
+        {
+            selectIP = int.Clamp(selectIP, 0, localhost.AddressList.Length);
+            ip = localhost.AddressList[selectIP];
+        }
+        
         int port = 2083;
 
         IPEndPoint endPoint = new(ip, port);
 
-        Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        using Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         Console.Title = endPoint.ToString();
         try
@@ -24,20 +52,24 @@ class Program
             while (true)
             {
                 Console.WriteLine("Successfully started the server, waiting for requests");
-                Socket client = socket.Accept();
+                using Socket client = await socket.AcceptAsync();
                 
                 Console.WriteLine($"Client end point {client.RemoteEndPoint}");
                 
                 int bytes = 0;
-                byte[] data = new byte[1024];
+                byte[] buffer = new byte[1024];
+                string clientMessage = "";
+                Console.WriteLine("Message from client: ");
                 do
                 {
-                    bytes = client.Receive(data);
-                    Console.WriteLine($"Message {Encoding.Unicode.GetString(data)}");
+                    bytes = client.Receive(buffer);
+                    clientMessage += Encoding.Unicode.GetString(buffer);
                 } while (client.Available > 0);
+                
+                Console.WriteLine(clientMessage);
 
-                string message = $"Thanks. Your request has been accepted {DateTime.Now}.";
-                data = Encoding.Unicode.GetBytes(message);
+                string message = $"Thanks. Your request `{clientMessage}` has been accepted {DateTime.Now}.";
+                byte[] data = Encoding.Unicode.GetBytes(message);
                 client.Send(data);
                 client.Shutdown(SocketShutdown.Both);
                 client.Close();
---
+++ 2. Simple Socket_Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _2._Simple_Socket_Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+        
+        Console.WriteLine("Enter the server's IP address: ");
+        var ip = IPAddress.Parse(Console.ReadLine());
+        Console.WriteLine("Enter port: ");
+        var port = int.Parse(Console.ReadLine());
+        try
+        {
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            using Socket server = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Connect(endPoint);
+            Console.Write("Enter your message to the server: ");
+            var message = Console.ReadLine();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            server.Send(data);
+            byte[] buffer = new byte[1024];
+            int bytes = 0;
+            Console.WriteLine("Server's response: ");
+            do
+            {
+                bytes = server.Receive(buffer);
+                Console.Write(Encoding.Unicode.GetString(buffer));
+            } while (server.Available > 0);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
