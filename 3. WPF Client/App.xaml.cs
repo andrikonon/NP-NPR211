7f6e1d10fe80f3eae3d0d03d149cbee24a9ca276
andrikonon <andri.konon@gmail.com>
2024-04-28 12:18:55 +0200

    Added GUI for client

---
+++ 3. WPF Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _3._WPF_Client;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ 3. WPF Client/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _2._Simple_Socket_Client;
+
+namespace _3._WPF_Client;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnSend_OnClick(object sender, RoutedEventArgs e)
+    {
+        var ip = IPAddress.Parse(txtServerIP.Text);
+        var port = int.Parse(txtServerPort.Text);
+
+        try
+        {
+            TCPClient client = new(new IPEndPoint(ip, port));
+            client.Send(txtMessage.Text);
+            var response = client.Recieve();
+            MessageBox.Show(response, "Відповідь", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(exception.Message, "Помилка");
+        }
+        
+    }
+}
